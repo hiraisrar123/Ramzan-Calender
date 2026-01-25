40c297f27da8e5fbe3e31dab535eec47abc8508c
hiraisrar123 <hiraisrar47@gmail.com>
2026-01-25 17:59:23 +0500

    first commit

---
+++ ass3/src/App.jsx
@@ -0,0 +1,32 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <div className="card">
+
+        <h2 className="arabic">رمضان مبارک</h2>
+        <h3 className="title">SAHRI & IFTAR TIMING - 2026</h3>
+
+        <div className="day">3</div>
+        <h3 className="date">27th APRIL 2026</h3>
+
+        <div className="timing-box">
+          <div className="time">
+            <h4>سحری</h4>
+            <p>04:46 AM</p>
+          </div>
+
+          <div className="time">
+            <h4>افطار</h4>
+            <p>07:04 PM</p>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
+ 
